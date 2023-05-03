fa105a9635f1c1621d45b1c6dbbe7fcde888abb9
VolodymyrMakarenko <118749504+VolodymyrMakarenko@users.noreply.github.com>
2023-05-03 20:03:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        string fileName = "C:\\Users\\Acer\\source\\repos\\Practik 6\\Practik 6\\input.txt"; // назва файлу з даними
+        int min = int.MaxValue; // мінімальне значення (ініціалізуємо максимальним значенням int)
+        int max = int.MinValue; // максимальне значення (ініціалізуємо мінімальним значенням int)
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int value = int.Parse(line); // перетворюємо рядок у ціле число
+                    if (value < min)
+                    {
+                        min = value;
+                    }
+                    if (value > max)
+                    {
+                        max = value;
+                    }
+                }
+            }
+            Console.WriteLine("Мінімальне значення: {0}", min);
+            Console.WriteLine("Максимальне значення: {0}", max);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Файл не знайдено.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Файл містить некоректні дані.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Сталася помилка: {0}", ex.Message);
+        }
+
+        Console.ReadKey();
+    }
+}
